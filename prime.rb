412cdf6e66ad4a0e34ebc11650c4a6fc9facb9a7
Christie Yoon <cleex1999@gmail.com>
2020-08-05 19:54:53 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,7 +1,7 @@
 require "pry"
 
 def prime?(number)
-  return false if !number.integer?
+  return true if !number.integer?
   return false if number < 2
   return true if number == 2
   (2...number-1).each {|int| false if number % int == 0}
